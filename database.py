c2b2a27ebcdccd1ea027a58302adee8a953a24e5
Neil Singh <neilsingh.miami@gmail.com>
2026-07-01 17:14:44 -0400

    added book insertion method

--- database.py
+++ database.py
@@ -31,3 +31,23 @@ try:
 except sqlite3.Error as error:
     print(f"error is {error}")
 
+def insertBook(title, genre, description, url):
+    try:
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("""
+                       INSERT OR IGNORE INTO books (title, genre, description, url)
+            VALUES (?, ?, ?, ?)
+                       """, (title, genre, description, url))
+
+        connection.commit()
+
+        cursor.execute("SELECT id FROM books WHERE url = ?", (url,))
+        bookID = cursor.fetchone[0] #get the book id
+        connection.close()
+        return bookID
+
+    except sqlite3.Error as error:
+        print(f"there is an error {error}")
+
+ 
