7ad84c318c6596f65319700bcfb2ebb4b38f8478
Neil Singh <neilsingh.miami@gmail.com>
2026-07-03 21:39:00 -0400

    got book id and descriptions, first step of implementing semantic search feature

--- generateEmbeddings.py
+++ generateEmbeddings.py
@@ -9,7 +9,14 @@ path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "database.db")
 model = SentenceTransformer("all-MiniLM-L6-v2") #set model type
 
 connection = sqlite3.connect(path)
+connection.row_factory = sqlite3.Row #access like dictionary instead of through indexes
+cursor = connection.cursor()
 
+cursor.execute("SELECT id, description FROM books") #get id and description from the database
+booksData = cursor.fetchall() #store the ids and descriptions
+
+
+    
 
 
 
