6d11fc339278a421b47d3024623236f5dba50305
Neil Singh <neilsingh.miami@gmail.com>
2026-07-07 19:47:04 -0400

    implemented algo for decoding embeddings and displaying books corresponding to users's request(semantic search accomplished)

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import json
 import os
 from flask import Flask, render_template, request, abort
 from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity #needed to compare two different embeddings(user and everything else in database)
 import numpy as np
 
 app = Flask(__name__)
 
 DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "database.db")
 
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def getDb():
     conn = sqlite3.connect(DB_PATH)
@@ -85,8 +86,61 @@ def bookDetail(bookId):
 @app.route("/search")
 def search():
     query = request.args.get("q", "")
-    return render_template("search.html", query=query)
+    results = []
+    connection = getDb() #establish connection to the database
+    cursor = connection.cursor()
 
+    cursor.execute("""
+                SELECT bookID, embedding FROM embeddings
+                   """)
+    everything = cursor.fetchall()
+    bookIDsFromTable = [row[0] for row in everything] #create list of just ids
+    embeddingsOfBooksFromTable = [row[1] for row in everything] #create list of embeddings
+
+    deconstructedBookIDs = []
+    deconstructedEmbeddings = []
+    for i in range(len(embeddingsOfBooksFromTable)):
+        deconstructedBookIDs.append(bookIDsFromTable[i]) #add id to list
+        decodedEmbedding = np.frombuffer(embeddingsOfBooksFromTable[i], dtype=np.float32) #convert from BLOB to array of floats(deconversion)
+        deconstructedEmbeddings.append(decodedEmbedding) #add decoded embedding to the list
+
+
+    if query:
+        encodedQuery = model.encode(query) #create embedding
+        encodedQuery = [encodedQuery] #turn into 2d array for cosine_similarity method
+        
+        collapsedArray = np.stack(deconstructedEmbeddings, axis=0) #arrays are stored horizontally
+        similarityScores = cosine_similarity(encodedQuery, collapsedArray) #run the cosine method to compare user embedding to the other embeddings
+        similarityScores = similarityScores[0] #get similarity scores for the user search
+
+        similarityScores = np.argsort(similarityScores)[::-1] #sort indexes by highest similarity to the lowest
+        topFiveSimilarity = similarityScores[:5] #get five highest similarity scores
+
+        topFiveBookIDs = []
+        for index in topFiveSimilarity:
+            topFiveBookIDs.append(deconstructedBookIDs[index]) #create list of top five bookids corresponding to the top five similarity scores
+
+
+        for id in topFiveBookIDs:
+            cursor.execute("""
+                           SELECT b.id, b.title, b.genre, p.price
+                            FROM books b
+                            JOIN prices p ON b.id = p.bookID
+                            WHERE b.id = ?
+                            AND p.id = (SELECT MAX(id) FROM prices WHERE bookID = b.id)
+                           """,(id,))
+            currentResults =  cursor.fetchone()
+            results.append({
+                "id": currentResults["id"],
+                "title": currentResults["title"],
+                "genre": currentResults["genre"],
+                "price": currentResults["price"] 
+            })
+ 
+    connection.close()
+    return render_template("search.html", query=query, results=results)
+    #does this run every time that a user searches???????
+    #yes it does, could be a performance issue if we scale up
 
 if __name__ == "__main__":
     app.run(debug=True)
